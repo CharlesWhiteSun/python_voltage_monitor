29dfc11f59e99be63f0bbf6a6991a71353a2f687
CharlesWhiteSun <charleswhitesun@gmail.com>
2025-09-30 05:57:46 +0800

    update setup tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="python_voltage_monitor",
-    version="1.1.2",
+    version="1.1.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
