c0beb11ddbfe4b3e586fc16775c2d01a3977bc68
CharlesWhiteSun <charleswhitesun@gmail.com>
2025-09-29 20:23:58 +0800

    fix setup encoding utf-8

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="python_voltage_monitor",
-    version="1.1.0",  # 請根據實際版本進行更新
+    version="1.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
-        "requests",  # 假設有這個依賴，請根據實際情況調整
+        "requests",
         # 其他必要的依賴套件
     ],
     description="Python application for retrieving voltage data from PLC devices and uploading it to the cloud for real-time energy monitoring.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/CharlesWhiteSun/python_voltage_monitor",
     author="Charles",
